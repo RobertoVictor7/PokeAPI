1cd074e54f5d9a9da5758207ea98922927b75f39
Victor <robertovictor0702@gmail.com>
2022-11-17 22:06:56 -0300

    Javascript add

--- src/script.js
+++ src/script.js
@@ -1,23 +1,46 @@
-const searchBtn = document.querySelector('#search');
-const pokeName = document.querySelector('.poke-name')
-const inputSearch = document.querySelector('.poke-search')
-
+const searchBtn = document.querySelector("#search");
+const pokeName = document.querySelector(".poke-name");
+const inputSearch = document.querySelector(".poke-search");
+const displayError = document.querySelector(".error-area");
 
 function handleClick(e) {
-    e.preventDefault();
-    const pokemon = inputSearch.value;
-    const formatPokemon = pokemon.toLowerCase();
-    searchPokemon(formatPokemon);
+  e.preventDefault();
+  const pokemon = inputSearch.value;
+  const formatPokemon = pokemon.toLowerCase();
+  searchPokemon(formatPokemon);
 }
 
 function searchPokemon(pokemon) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.name)
+  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const pokemon = {
+        displayPokemon: document.querySelector(".pokemon-display img"),
+        displayName: document.querySelector(".pokemon-name"),
+        displayType: document.querySelector(".display-type"),
+        displayHeight: document.querySelector(".display-height"),
+        displayWeight: document.querySelector(".display-weight"),
+      };
+
+      pokemon.displayPokemon.src = data.sprites.front_default;
+      pokemon.displayName.innerText = data.name;
+      pokemon.displayHeight.innerText = data.height;
+      pokemon.displayWeight.innerText = data.weight;
+
+      const type = data.types;
+      const pokemonTypes = type.map((type) => {
+        return (type = type.type.name);
+      });
+
+      pokemon.displayType.innerText = pokemonTypes;
+      displayError.innerText = '';
     })
-    
+    .catch((err) => {
+      
+      err.message = "Pokemon not found";
+      const error = err.message;
+      displayError.innerText = error;
+    });
 }
 
-searchBtn.addEventListener('click', handleClick);
-
+searchBtn.addEventListener("click", handleClick);
